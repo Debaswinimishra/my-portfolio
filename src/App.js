fd36f116f81e44b1f147a3d9afd5c01f3c80c73f
Debaswinimishra <debaswinimishra@gmail.com>
2024-04-12 12:23:24 +0530

    design add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./containers/home";
+import About from "./containers/about";
+import Contact from "./containers/contact";
+import Skills from "./containers/skills";
+import Portfolio from "./containers/portfolio";
+import Resume from "./containers/resume";
+import Navbar from "./components/navBar";
 
 function App() {
   return (
     <div className="App">
-      <h1>my portfoilo</h1>
+      {/* navbar */}
+      <Navbar />
+      {/* main page content */}
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/navBar/index.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { FaReact } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const data = [
+  {
+    label: "HOME",
+    to: "/",
+  },
+  {
+    label: "ABOUT ME",
+    to: "/about",
+  },
+  {
+    label: "SKILLS",
+    to: "/skills",
+  },
+  {
+    label: "RESUME",
+    to: "/resume",
+  },
+  {
+    label: "PORTFOLIO",
+    to: "/portfolio",
+  },
+  {
+    label: "CONTACT",
+    to: "/contact",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar__container">
+          <Link to={"/"} className="navbar__container__logo">
+            <FaReact size={30} />
+          </Link>
+        </div>
+        <ul className="navbar__container__menu">
+          {data.map((item, key) => (
+            <li key={key} className="navbar__container__menu__item">
+              <Link
+                className="navbar__container__menu__item__links"
+                to={item.to}
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+};
+export default Navbar;
